470917d46d02ab58d0b28fbbbe1d8081a4f1182f
Diego Ramirez <gustavormzrmz@gmail.com>
2022-05-07 16:15:36 -0500

    created utils file; added calculateFibonacciResultByTerm function

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+const calculateFibonacciResultByNTerm = ({
+  nTerm
+}) => {
+  let result = 0;
+  let currentNumber = 1;
+  let auxNumber = 1;
+
+  for (let index = 0; index < nTerm; index++) { 
+    auxNumber = currentNumber;
+    currentNumber = result;
+    result = currentNumber + auxNumber;
+  }
+
+  return result;
+};
+
+export default {
+  calculateFibonacciResultByNTerm
+};
