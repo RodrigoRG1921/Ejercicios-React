c9808ba3eac6590c2f9d0ef38a9005bb00b5e065
Diego Ramirez <gustavormzrmz@gmail.com>
2022-05-08 12:29:14 -0500

    reset master branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Fibonacci from './components/fibonacci';
-import { calculateFibonacciResultByNTerm } from './utils';
-
 const App = () => {
-  const [nTermFibonacci, setNTermFibonacci] = useState(1);
-
-  useEffect(() => {
-    const intervalReference = setInterval(() => {
-      setNTermFibonacci(_nTermFibonacci => _nTermFibonacci + 1);
-    }, 1000);
-
-    return () => clearInterval(intervalReference);
-  }, []);
-
   return (
     <>
-      <Fibonacci fibonacciNumber={ calculateFibonacciResultByNTerm({ nTerm: nTermFibonacci }) }/>
     </>
   );
 };
