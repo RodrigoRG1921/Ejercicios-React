9928ebe6b220bc4493c1eadb3e8d8d6f0eaf014a
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-05-08 16:24:21 -0500

    Initial data

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const MOVIES = [
+    { name: "Harry Potter" , duration: 10,  year: 1990 },
+    { name: "Predestination" , duration: 120,  year: 2001 },
+    { name: "The lord of the rings" , duration: 30,  year: 2001 },
+    { name: "Yo robot" , duration: 30,  year: 2024 },
+    { name: "Exmachine" , duration: 30,  year: 1998 },
+    { name: "Matrix" , duration: 80,  year: 1998 },
+    { name: "Soy leyenda" , duration: 70,  year: 2006 },
+    { name: "Triangulo" , duration: 100,  year: 2006 },
+    { name: "Titanes del pacifico" , duration: 110, year: 2005}
+    ];
+
+    export default MOVIES
